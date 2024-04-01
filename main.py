8c266e16ec951388c3d295030fd446915c81b279
Stas Sergheev <sergheev.stanislav@usm.md>
2024-04-01 18:41:30 +0300

    commit

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-text = 'Hello World'
+text = 'Hello Zaira'
 shift = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-encrypted_text = ''
 
-for char in text.lower():
-    if char == ' ':
-        encrypted_text += char
-    else:
-        index = alphabet.find(char)
-        new_index = index + shift
-        encrypted_text += alphabet[new_index]
-    print('char:', char, 'encrypted text:', encrypted_text)
+def caesar():
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in text.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + shift) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+            
+    print('plain text:', text)
+    print('encrypted text:', encrypted_text)
+    
+caesar()
