177086c95e3db036f1a24faba0e8a448ce38b51d
Stas Sergheev <sergheev.stanislav@usm.md>
2024-04-02 17:18:24 +0300

    done!

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-text = 'Hello Zaira'
-custom_key = 'python'
+text = 'mrttaqrhknsw ih puggrur'
+custom_key = 'happycoding'
 
-def vigenere(message, key):
+def vigenere(message, key, direction=1):
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+    final_message = ''
 
     for char in message.lower():
-    
-        # Append space to the message
-        if char == ' ':
-            encrypted_text += char
+
+        # Append any non-letter character to the message
+        if not char.isalpha():
+            final_message += char
         else:        
-            # Find the right key character to encode
+            # Find the right key character to encode/decode
             key_char = key[key_index % len(key)]
             key_index += 1
-            # Define the offset and the encrypted letter
+
+            # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
             index = alphabet.find(char)
-            new_index = (index + offset) % len(alphabet)
-            encrypted_text += alphabet[new_index]
-    print('plain text:', message)
-    print('encrypted text:', encrypted_text)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
+    
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+
+print(f'\nEncrypted text: {text}')
+print(f'Key: {custom_key}')
+decryption = decrypt(text, custom_key)
+print(f'\nDecrypted text: {decryption}\n')
